1034f2802c4cd32f01d1cc02f59c138351869a91
Tom <thomas.j.busillo@gmail.com>
2022-06-21 23:41:24 -0400

    adds utils folder and key_codes constant helper

---
+++ utils/KEY_CODES.js
@@ -0,0 +1,21 @@
+/* Example Usage */
+//
+// import KEY_CODES from '<dir>/KEY_CODES';
+//
+/* var keyCode = KEY_CODES.ENTER; */
+
+export default Object.freeze({
+  SHIFT_LEFT: 'ShiftLeft',
+  SPACEBAR: 'Space',
+  LEFT_ARROW: 'ArrowLeft',
+  RIGHT_ARROW: 'ArrowRight',
+  DOWN_ARROW: 'ArrowDown',
+  UP_ARROW: 'ArrowUp',
+  HOME: 'Home',
+  END: 'End',
+  PAGE_UP: 'PageUp',
+  PAGE_DOWN: 'PageDown',
+  TAB: 'Tab',
+  ENTER: 'Enter',
+  ESCAPE: 'Escape'
+});
